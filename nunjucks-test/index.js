5f81418921d720571ee09a4be62309f138bffc81
Harminder Virk <virk.officials@gmail.com>
2017-03-18 10:23:01 +0530

    refactor: remove unwanted files

--- nunjucks-test/index.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const nunjucks = require('nunjucks')
-const path = require('path')
-const viewsDir = path.join(__dirname, './')
-nunjucks.configure(viewsDir)
-
-console.log(nunjucks.precompile(path.join(viewsDir, './simple.njk')))
