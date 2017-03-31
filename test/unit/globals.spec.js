7ddbb1eedce437dadbf54faebde91cef2e7292ab
Harminder Virk <virk.officials@gmail.com>
2017-03-31 14:27:08 +0530

    feat(globals): add globals

---
+++ test/unit/globals.spec.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const test = require('japa')
+const Globals = require('../../src/Globals')
+
+test.group('Globals', () => {
+  test('return safe element object when .el global is called', (assert) => {
+    const context = {
+      safe (input) {
+        return input
+      }
+    }
+    const output = Globals.el.bind(context)('<a href="$url"> $title </a>', { title: 'Docs', url: '/docs' })
+    assert.equal(output, '<a href="/docs"> Docs </a>')
+  })
+
+  test('replace with nested objects', (assert) => {
+    const context = {
+      safe (input) {
+        return input
+      }
+    }
+    const output = Globals.el.bind(context)('<a href="$meta.url"> $meta.title </a>', { meta: {title: 'Docs', url: '/docs'} })
+    assert.equal(output, '<a href="/docs"> Docs </a>')
+  })
+
+  test('replace with nested objects inside an array', (assert) => {
+    const context = {
+      safe (input) {
+        return input
+      }
+    }
+    const output = Globals.el.bind(context)('<a href="$meta.0.url"> $meta.0.title </a>', { meta: [{title: 'Docs', url: '/docs'}] })
+    assert.equal(output, '<a href="/docs"> Docs </a>')
+  })
+
+  test('convert a link to an anchor tag', (assert) => {
+    const context = {
+      safe (input) {
+        return input
+      }
+    }
+    const output = Globals.toAnchor.bind(context)('http://google.com')
+    assert.equal(output, '<a href="http://google.com"> http://google.com </a>')
+  })
+
+  test('convert a link to an anchor tag with custom title', (assert) => {
+    const context = {
+      safe (input) {
+        return input
+      }
+    }
+    const output = Globals.toAnchor.bind(context)('http://google.com', 'Google')
+    assert.equal(output, '<a href="http://google.com"> Google </a>')
+  })
+
+  test('encode url', (assert) => {
+    const output = Globals.urlEncode('http://foo.com?username=aman virk')
+    assert.equal(output, 'http://foo.com?username=aman%20virk')
+  })
+})
