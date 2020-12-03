8c55087a211d7a089f0ffd30fb74b43864afffe4
Harminder Virk <virk.officials@gmail.com>
2020-12-03 19:56:01 +0530

    test: add test for nested components

---
+++ fixtures/nested-components/components/compiled.js
@@ -0,0 +1,17 @@
+let out = "";
+let $lineNumber = 1;
+let $filename = "{{__dirname}}index.edge";
+try {
+out += template.renderWithState("components/alert", {}, { main: function () {
+let slot_main = "";
+try {
+slot_main += "  Hello world";
+} catch (error) {
+ctx.reThrow(error, $filename, $lineNumber);
+}
+return slot_main;
+} }, { filename: $filename, line: $lineNumber, col: 0 });
+} catch (error) {
+ctx.reThrow(error, $filename, $lineNumber);
+}
+return out;
